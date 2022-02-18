eed6ff35ba33c0b9d70738016922fcba6a94e5d8
Stanislau Parechyn <stasparechin@icloud.com>
2022-02-18 12:13:43 +0300

    Remove only debug code

--- Sources/UIClient+Toast/UIClient+Toast.swift
+++ Sources/UIClient+Toast/UIClient+Toast.swift
@@ -8,15 +8,12 @@ import UIClient
 extension UIClient {
     public func showToast(_ model: ToastModel) -> Effect<Never, Never> {
         .fireAndForget {
-            #if RELEASE
-            #else
             guard let window = UIApplication.shared.windows.first(where: \.isKeyWindow) else { return }
             window.rootViewController?.view
                 .makeToast(model.message,
                            duration: model.duration,
                            position: model.position,
                            title: model.title)
-            #endif
         }
     }
 }
